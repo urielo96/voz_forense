b50213ca54d312d5f6480a6e58295745d50c93ca
iansvein <iansarteaga@gmail.com>
2026-06-29 10:12:20 -0600

    Fix typos in intensity mode and column names

--- extraccion/services.py
+++ extraccion/services.py
@@ -63,7 +63,7 @@ def extract_interval_features_s(sound, intensity, intensity_mode, start, stop, c
     #intensidad
     if (intensity_mode == "mean"):
         intensity_value = parselmouth.praat.call(intensity, "Get mean", start, stop, 'energy')
-    elif (intensity_mode == "maximun"):
+    elif (intensity_mode == "maximum"):
         intensity_value = parselmouth.praat.call(intensity, "Get maximum", start, stop, "Parabolic")
     else:
         print( "Error: No se seleccionó ningún párametro." )
@@ -127,13 +127,13 @@ def process_file(file_path, genero, pitch_mode, formant_mode, intensity_mode, ce
     # Definir los nombres de columna para el DataFrame
     column_names = ['name','Label','Start', 'Stop','Duration', pitch_mode + '_Pitch'] + formantes + [intensity_mode + '_Intensity']
     if center_b:
-        column_names.append('Center_spectrum')
+        column_names.append('Centro de gravedad')
     if sd_b:
-        column_names.append('SD_spectrum')
+        column_names.append('Desviación estándar')
     if sk_b:
-        column_names.append('SK_spectrum')
+        column_names.append('Asimetría')
     if kur_b:
-        column_names.append('KUR__spectrum')
+        column_names.append('Curtosis')
     if center_b and sk_b:
         column_names.append('Altura_friccion')
 
